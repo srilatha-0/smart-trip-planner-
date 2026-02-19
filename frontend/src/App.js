707281ad254e3feb557e3e3e687f8fb0691dd1b1
srilatha-0 <srilathapamula4@gmail.com>
2026-02-19 19:08:58 +0530

    added the npm to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import HomePage from "./components/HomePage";
-import DiscoverTrips from "./components/DiscoverTrips";
+
+import DiscoverTrips from "./pages/discoverTrips/DiscoverTrips";
+import HomePage from "./pages/home/HomePage";;
 
 function App() {
   return (
